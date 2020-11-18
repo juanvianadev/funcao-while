46fcf423d9cece831e7f04cc843c83223a24dc01
Juan <juanviana2577@gmail.com>
2020-11-18 09:24:11 -0300

    While entendimentos

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace funcao_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo WHILE");
+
+            // Console.WriteLine("Digite um número");
+            // int numero = int.Parse(Console.ReadLine());
+
+            //int contador = 0;
+
+            // while (contador <= numero)
+            // {
+            // Console.WriteLine(contador);
+            // contador++;
+            // }
+            
+            // while (i <= numero){
+            //     Console.WriteLine(i);
+            //     i = i + 0.01m;
+            // }
+
+            // string resposta = "";
+            // do{
+            //     Console.WriteLine("Digite seu email");
+            //     resposta = Console.ReadLine();
+            // }
+
+            // while(resposta != "juanviana@gmail.com");
+
+            // Console.WriteLine("email confirmado");
+
+            // Console.WriteLine("Digite a idade do ator");
+            // int idade = int.Parse(Console.ReadLine());
+
+            // while(idade != 47){
+            // Console.WriteLine("Errou!");
+            // Console.WriteLine("Digite a idade do ator");
+            // idade = int.Parse(Console.ReadLine());
+            // }
+            // Console.WriteLine("Acertou!");
+
+            // int idade;
+            // do{
+            // Console.WriteLine("Digite a idade do ator");
+            // idade = int.Parse(Console.ReadLine());
+            // }
+            // while(idade != 47);
+            // Console.WriteLine("Acertou!");
+            // }
+
+            //for (var contador = 0; contador < 10; contador++){
+            //Console.WriteLine("Digite um valor");
+            //int valor = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Você digitou: {valor}");
+            //Console.WriteLine("---------------");
+            //}
+
+
+            
+        }
+    }
+}
